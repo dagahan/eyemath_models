eeee0b673f5a1cae7eac538a4b2e87ee284db966
dagahan <nikitosu27@gmail.com>
2025-08-13 22:59:42 +0700

    19

--- bs_schemas/jwt.py
+++ bs_schemas/jwt.py
@@ -2,13 +2,13 @@ from .base_schema import *
 
 
 class AccessPayload(BaseModel):
-    sub: int  # user_id
+    sub: str  # user_id
     sid: str  # session_id
     exp: int  # expiry timestamp
 
 
 class RefreshPayload(BaseModel):
-    sub: int
+    sub: str
     sid: str
     exp: int
     ref: bool # refresh token 
