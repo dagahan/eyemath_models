31cb84428a35118ef25b5c03f73dd89da0873d5a
dagahan <nikitosu27@gmail.com>
2025-08-19 23:34:54 +0700

    66

--- bs_schemas/endpoints/categories.py
+++ bs_schemas/endpoints/categories.py
@@ -3,7 +3,7 @@ from ..product_type import ProductTypeDTO
 
 
 class CategoriesRequest(BaseModel):
-    categories: Optional[List[BookCategory]] = None
+    categories: Optional[List[ProductTypeCategory]] = None
 
 
 class CategoriesResponse(BaseModel):
